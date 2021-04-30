2a69f054557d8f3d79cfa83b6efb3a59551c6296
Rachèl Heimbach <rachel.heimbach@rabobank.nl>
2021-04-30 21:37:47 +0200

    Added styled-components + theming

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import "../globals.css";
+import { ThemeProvider } from "styled-components";
+import { AppProps } from "next/app";
+import { defaultTheme } from "../theme/default";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
---
+++ src/theme/default.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const defaultTheme: DefaultTheme = {
+  colors: {
+    primary: "#59C9A5",
+    secondary: "#DAD2D8",
+  },
+};
