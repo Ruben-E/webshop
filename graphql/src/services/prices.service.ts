4a1f1094bf1470f993407c7544e4355ad84ae67b
Rachèl Heimbach <rachel.heimbach@rabobank.nl>
2021-05-15 15:41:27 +0200

    Price data loader

--- graphql/src/services/prices.service.ts
+++ graphql/src/services/prices.service.ts
@@ -1,9 +1,27 @@
 import fetch from "node-fetch";
+import DataLoader from "dataloader";
+
+const PRICES_ENDPOINT = `http://localhost:3000/api/prices`;
 
 export class PricesService {
+  private priceDataLoader = new DataLoader<number, number>(
+    async (ids: number[]) =>
+      ids.length === 1
+        ? [
+            await fetch(`${PRICES_ENDPOINT}/${ids[0]}`).then((res) =>
+              res.json()
+            ),
+          ]
+        : this.getByIds(ids)
+  );
+
   async getById(id: number): Promise<number> {
-    return fetch(`http://localhost:3000/api/prices/${id}`).then((res) =>
-      res.json()
-    );
+    return this.priceDataLoader.load(id);
+  }
+
+  async getByIds(ids: number[]): Promise<number[]> {
+    return fetch(`${PRICES_ENDPOINT}?ids=${ids.join(",")}`)
+      .then((res) => res.json())
+      .then((data) => data.map(({ price }) => price));
   }
 }
