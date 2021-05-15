22700cf65d731f8d9498e4758888439c2c6e539b
Rachèl Heimbach <rachel.heimbach@rabobank.nl>
2021-05-15 22:02:06 +0200

    Api paging support

--- src/pages/api/items/index.ts
+++ src/pages/api/items/index.ts
@@ -6,14 +6,22 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case "GET": {
       const ids = req.query.ids as string;
+      const page = req.query.page as string;
+      const size = req.query.size as string;
       let items: RemoteItem[];
       if (ids) {
         items = ids.split(",").map((id) => NORMALIZED_ITEMS[id]);
       } else {
-        items = ITEMS;
+        items = ITEMS.slice(0);
+      }
+      if (page && size) {
+        const pageInt = parseInt(page);
+        const sizeInt = parseInt(size);
+        const start = pageInt * sizeInt;
+        items = items.splice(start, sizeInt);
       }
       res.status(200).json(items);
-      console.debug(`/api/items returned`, items.length, 'items');
+      console.debug(`/api/items returned`, items.length, "items");
       break;
     }
     default: {
