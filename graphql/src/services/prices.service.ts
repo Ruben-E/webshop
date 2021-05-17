bd7257b74c0885650f75b38149cccb5ac06e7106
Rachèl Heimbach <rachel.heimbach@rabobank.nl>
2021-05-17 22:15:44 +0200

    2.2 Solved N+1 problem with Dataloader + Bonus

--- graphql/src/services/prices.service.ts
+++ graphql/src/services/prices.service.ts
@@ -1,8 +1,26 @@
 import fetch from "node-fetch";
+import DataLoader from "dataloader";
+
+const PRICES_ENDPOINT = 'http://localhost:3000/api/prices';
 
 export class PricesService {
+  private priceDataLoader = new DataLoader<number, number>(
+    async (ids: number[]) =>
+      ids.length === 1
+        ? [
+            await fetch(`${PRICES_ENDPOINT}/${ids[0]}`).then((res) =>
+              res.json()
+            ),
+          ]
+        : this.getByIds(ids)
+  );
+
   async getById(id: number): Promise<number> {
-    return await fetch(`http://localhost:3000/api/prices/${id}`).then((res) =>
+    return this.priceDataLoader.load(id);
+  }
+
+  async getByIds(ids: number[]): Promise<number[]> {
+    return fetch(`${PRICES_ENDPOINT}?ids=${ids.join(",")}`).then((res) =>
       res.json()
     );
   }
